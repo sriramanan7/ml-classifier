0b10b09d211d82ffa8e9ba6ceaff44ef1f32d7e2
Sri Ramanan K <32416420+sriramanan7@users.noreply.github.com>
2026-02-08 18:54:01 +0530

    Cosmetic changes in app.py for confusion matrix

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if data is not None:
                     with col2:
                         st.write("#### Confusion Matrix")
                         cm = confusion_matrix(y_test, y_pred)
-                        fig, ax = plt.subplots(figsize=(20, 10))
+                        fig, ax = plt.subplots(figsize=(10, 4))
                         sns.heatmap(cm, annot=True, fmt='d', cmap='Reds', ax=ax)
                         st.pyplot(fig)
                         
@@ -119,6 +119,7 @@ if data is not None:
         
 else:
     st.info("Please select a data source (Upload or Demo) to proceed.")
+
 
 
 
